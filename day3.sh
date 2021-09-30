2a1e2e53067425465e9e13dd79dcdccbed5e5554
markysparky1 <markysparky@protonmail.com>
2021-09-30 16:19:18 +0100

    adding 3 files

---
+++ day3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#This script is for variable practice
+
+WORD="script"
+echo "${WORD}ing is fun!"
+
+# change the word stored in the variable WORD - reassignment
+WORD="read"
+echo "${WORD}ing is fun!"
